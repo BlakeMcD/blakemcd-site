74df7cb824522aa74402a06db2900be0ba8c9ae9
Blake <blakemcd@gmail.com>
2022-01-06 20:23:02 +1100

    got animation working on click for parent 1

--- script.js
+++ script.js
@@ -10,6 +10,17 @@ const parent4 = document.querySelector('.parent-4');
 
 // CLICK ON PARENT ICON
 parent1.addEventListener('click', () => {
-    console.log("click registered")
-    heading.innerHTML="It's working!"
+    if (parentClicked !== 1) {
+    parentClicked = 1;
+    console.log("click registered"+`${parentClicked}`);
+    heading.innerHTML="It's working!";
+    parent1.classList.add("parent-1Activated");
+    parent1.classList.remove("parent-1DisActivated");
+    }
+    else {
+        parentClicked = 0;
+        console.log("click registered"+`${parentClicked}`);
+        parent1.classList.remove("parent-1Activated");
+        parent1.classList.add("parent-1DisActivated");
+    }
 })
