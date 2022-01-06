e49640d54415254feee06e9e4c599344658efc42
Blake <blakemcd@gmail.com>
2022-01-06 19:58:42 +1100

    got clicking on parent registering

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
 // DECLARE VARIABLES
+let parentClicked = 0;
+
+const heading = document.querySelector("h1");
+
+const parent1 = document.querySelector('.parent-1');
+const parent2 = document.querySelector('.parent-2');
+const parent3 = document.querySelector('.parent-3');
+const parent4 = document.querySelector('.parent-4');
+
+// CLICK ON PARENT ICON
+parent1.addEventListener('click', () => {
+    console.log("click registered")
+    heading.innerHTML="It's working!"
+})
